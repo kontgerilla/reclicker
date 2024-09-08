f594c840294da4ea249304bccc64cae94fcf6bf3
Kontrgerilla <67336740+kontgerilla@users.noreply.github.com>
2024-09-08 22:56:57 +0300

    Update clicker-run.py

--- clicker-run.py
+++ clicker-run.py
@@ -24,8 +24,8 @@ from colorama import Fore, Style, init
 
 init(autoreset=True)
 exit_ = False
-
 mouse = Controller()
+
 def get_user_input():
     """Get user input for number of repeats, time between clicks and time between sets of clicks."""
     while True:
@@ -34,10 +34,7 @@ def get_user_input():
             time_out = int(input(Fore.YELLOW + "How many seconds between each click? "))
             repeat_frequency = int(input(Fore.YELLOW + "How many seconds between each set of clicks? "))
             return repeat_count, time_out, repeat_frequency
-        except ValueError:
-            print(Fore.RED + "Invalid input. Please try again.")
-            continue
-
+        except ValueError:print(Fore.RED + "Invalid input. Please try again.");continue
 
 def load_mouse_positions():
     """Load mouse positions from file."""
@@ -45,14 +42,11 @@ def load_mouse_positions():
     with open("mouse_log.txt", "r") as file:
         for line in file.readlines():
             x, y, button = line.strip().split(',')
-
             positions.append((int(x), int(y),str(button)))
     print(Fore.GREEN + f"Loaded {len(positions)} mouse positions from file.")
     return positions
 
-
 def perform_clicks(positions, repeat_count, time_out, repeat_frequency):
-
     """Perform mouse clicks at specified intervals."""
     for i in range(repeat_count):
         print(Fore.MAGENTA + f"\n{i+1}. set of clicks starting...")
@@ -60,18 +54,12 @@ def perform_clicks(positions, repeat_count, time_out, repeat_frequency):
         for pos in positions:
             mouse.position = (pos[0], pos[1])
             if 'left' in pos[2]:
-                if exit_:
-                    print(Fore.RED+ "\nexiting...")
-                    time.sleep(2)
-                    exit()
+                if exit_: print(Fore.RED+ "\nexiting...");time.sleep(2);exit()
                 mouse.click(Button.left, 1)  # Left clickqqq
                 print(Fore.CYAN + f"Left click at ({pos[0]}, {pos[1]})")
                 time.sleep(time_out)  # Wait between clicks
             elif 'right' in pos[2]:
-                if exit_:
-                    print(Fore.RED+ "\nexiting...")
-                    time.sleep(2)
-                    exit()
+                if exit_:print(Fore.RED+ "\nexiting...");time.sleep(2);exit()
                 mouse.click(Button.right, 1)  # Right click
                 print(Fore.CYAN + f"Right click at ({pos[0]}, {pos[1]})")
                 time.sleep(time_out)  # Wait between clicks
@@ -87,14 +75,9 @@ def perform_clicks(positions, repeat_count, time_out, repeat_frequency):
 def on_press(key):
     global exit_
     """Stop script on 'esc' key press."""
-    if key == Key.esc:
-        print(Fore.YELLOW + "Stopping script.")
-        exit_ = True
-        return False  # Stop listener
+    if key == Key.esc: print(Fore.YELLOW + "Stopping script."); exit_ = True; return False  # Stop listener
 
 if __name__ == "__main__":
     repeat_count, time_out, repeat_frequency = get_user_input()
     positions = load_mouse_positions()
-    with KeyboardListener(on_press=on_press) as listener:
-        perform_clicks(positions, repeat_count, time_out, repeat_frequency)
-        listener.join()
+    with KeyboardListener(on_press=on_press) as listener: perform_clicks(positions, repeat_count, time_out, repeat_frequency); listener.join()
